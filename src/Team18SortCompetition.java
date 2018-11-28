fe311c5f3f68e774b05f2d28123f9798d7d30311
BT_1N3_31 <@>
2018-11-28 09:23:53 -0500

    afueufueufeufuefuefueu

---
+++ src/Team18SortCompetition.java
@@ -0,0 +1,76 @@
+public abstract class Team18SortCompetition extends SortCompetition
+{
+    public int challengeOne(int[] arr)
+    {
+        for(int i = 0; i < arr.length; i++)
+        {
+
+        }
+        int i = 1;
+        return i;
+    }
+    public static void intSwap(int[] arr, int pos1, int pos2){
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+
+    public static void doubleSwap(double[] arr, int pos1, int pos2){
+        double temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+
+    public static void bubbleSort(String[] arr)
+    {
+        boolean checkStorted = false;
+        int a = arr.length-1;
+        while(!checkStorted)
+        {
+            checkStorted = true;
+            for(int i=0; i<a; i++)
+            {
+                if(arr[i].compareTo(arr[i+1]) > 0 )
+                {
+                    String Temp = arr[i+1];
+                    arr[i+1] = arr[i];
+                    arr[i] = Temp;
+                    checkStorted = false;
+                }
+            }
+            a--;
+        }
+    }
+
+    public static void insertionSort(int[] arr)
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                for(int j=i ; j>0 ; j--)
+                {
+                    if(arr[i]<arr[j])
+                    {
+                        intSwap(arr,j,i);
+                    }
+                    else { break; }
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(double[] arr)
+    {
+        int temp;
+        for(int i=0;i<arr.length;i++){
+            temp = i;
+            for(int j=i+1;j<arr.length;j++) {
+                if (arr[temp] > arr[j]) {
+                    temp = j;
+                }
+            }
+            doubleSwap(arr, temp, i);
+        }
+    }
+}
