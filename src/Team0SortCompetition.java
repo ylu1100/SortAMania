496fbd005048678aeb4af6480b4b92a92ea06ef2
BT_1N3_02 <byu150@bths.edu>
2018-11-28 09:36:52 -0500

    bubl

--- src/Team0SortCompetition.java
+++ src/Team0SortCompetition.java
@@ -1,7 +1,31 @@
 public class Team0SortCompetition extends SortCompetition {
     @Override
     public int challengeOne(int[] arr) {
-
+        public static void bubbleSort(String[] list1)
+        {
+            String temp;
+            int swaps = 0;
+            boolean swapped = true;
+            while(swapped) {
+                for (int i = 0; i < list1.length - 1; i++) {
+                    if (list1[i].compareTo(list1[i + 1]) > 0) {
+                        temp = list1[i];
+                        list1[i] = list1[i + 1];
+                        list1[i + 1] = temp;
+                        swaps++;
+                    }
+                }
+                if(swaps > 0)
+                {
+                    swaps = 0;
+                }
+                else
+                {
+                    if(swaps == 0)
+                        swapped = false;
+                }
+            }
+        }
     }
 
     @Override
