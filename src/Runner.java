02282fd2291c16e5eb98fb46b6930df5779ec55e
BT_1N3_04 <vli8558@bths.edu>
2018-11-28 09:15:38 -0500

    runner

---
+++ src/Runner.java
@@ -0,0 +1,30 @@
+public class Runner {
+    public static void main(String[] args)
+    {
+
+    }
+    public static int[] generateIntArr(int x)
+    {
+        int[] z = new int[x];
+        for(int i=0;i<x;i++)
+        {
+            z[i] = (int)(Math.random()*10000);
+        }
+        return z;
+    }
+    public static String[] randomStringArr(int num, int length) {
+        String[] arr = new String[num];
+        while (num > 0) {
+            int i = 0;
+            String s = "";
+            while (i < length) {
+                char c = (char) ((Math.random() * 26) + 97);
+                s = s + c;
+                i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return arr;
+    }
+}
