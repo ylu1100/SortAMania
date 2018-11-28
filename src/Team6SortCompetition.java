72302179eea9e78bf1bd6157d82e7ea83c8cf1a1
clam3154 <clam3154@bths.edy>
2018-11-28 10:15:28 -0500

    so far

--- src/Team6SortCompetition.java
+++ src/Team6SortCompetition.java
@@ -1,30 +1,55 @@
 public class Team6SortCompetition extends SortCompetition{
 
-    public int challengeOne(int[] arr) {
-        //return median;
+    public int challengeOne(int[] arr)
+    {
+        int i = 0;
+        int j = arr.length-1;
+        int median;
+        int[] sortedArr = quickSort(arr,0,arr.length-1);
+        while(i != j)
+        {
+            i++;
+            j--;
+            if((i+1) == j || (j-1) == i)
+            {
+                int total = sortedArr[i] + sortedArr[j];
+                median = (total/2);
+                break;
+            }
+            else
+            {
+                median = sortedArr[i];
+            }
+        }
+        return median;
     }
 
-    public int challengeTwo(String[] arr, String query) {
+    public int challengeTwo(String[] arr, String query)
+    {
         //return index of the first instance of that string, or -1 if not found;
     }
 
-    public int challengeThree(int[] arr) {
+    public int challengeThree(int[] arr)
+    {
         //return median;
     }
 
-    public int challengeFour(int[][] arr) {
+    public int challengeFour(int[][] arr)
+    {
         //return median of the median array;
     }
 
-    public int challengeFive(Comparable[] arr, Comparable query) {
+    public int challengeFive(Comparable[] arr, Comparable query)
+    {
         //return position of the object, or -1 if not found;
     }
 
-    public String greeting() {
+    public String greeting()
+    {
         //return any;
     }
 
-    public void quickSort(int[] arr, int left, int right)
+    public int[] quickSort(int[] arr, int left, int right)
     {
         if(left<right)
         {
@@ -32,11 +57,6 @@ public class Team6SortCompetition extends SortCompetition{
             quickSort(arr,left,pivot-1);
             quickSort(arr,pivot+1,right);
         }
-        for(int i = 0; i<arr.length; i++)
-        {
-            System.out.print(arr[i] + ",");
-        }
-        System.out.println();
     }
 
     public int partition(int[] arr, int left, int right)
