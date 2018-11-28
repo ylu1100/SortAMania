49c08b1f8072dc929a526e837e23957b08112b03
BT_1N3_18 <pyu6253@bths.edu>
2018-11-28 09:33:32 -0500

    challengeOne

---
+++ src/Team10SortCompetition.java
@@ -0,0 +1,25 @@
+public abstract class Team10SortCompetition extends SortCompetition {
+    public int challengeOne(int[] arr)
+    {
+        int temp1 = 0;
+        int temp2 = 0;
+        int temp3 = 0;
+        int median = 0;
+        for (int i = 0;i<arr.length;i++)
+        {
+            temp3 = arr[i+1];
+            temp1 = i;
+            for(int x = i;x<arr.length;x++)
+            {
+                if (arr[i]<temp2)
+                {
+                    temp1 = x;
+                    temp2 = arr[x];
+                }
+            }
+            arr[temp1] = arr[i];
+            arr[i] = temp2;
+        }
+
+    }
+}
