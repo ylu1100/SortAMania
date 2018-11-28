8f792497b3942a19160f38b372862c0944c76393
BT_1N3_03 <n>
2018-11-28 09:16:03 -0500

    b

---
+++ src/Team1SortCompetition.java
@@ -0,0 +1,21 @@
+public class Team1SortCompetition extends SortCompetition {
+    public int challengeOne (int[] arr){
+
+    }
+    public int challengeTwo (String[] arr, String query){
+
+    }
+    public int challengeThree (int[] arr){
+
+    }
+    public int challengeFour (int[][] arr){
+
+    }
+    public int challengeFive (Comparable[] arr, Comparable query){
+
+    }
+
+    public String greeting(){
+        return "Hello.";
+    }
+}
