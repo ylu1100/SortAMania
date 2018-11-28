1a13752140f9da310b712f8275eda762fe89fb80
BT_1N3_01 <@>
2018-11-28 09:34:23 -0500

    1

--- src/Team0SortCompetition.java
+++ src/Team0SortCompetition.java
@@ -1,57 +1,6 @@
 public class Team0SortCompetition extends SortCompetition {
     @Override
-    public int challengeOne(int[] arr)
-    {
-        int n = arr.length;
-        int[] temp = new int[n];
-        mergeSortHelper(arr, 0, n-1, temp);
-
-        public void mergeSortHelper(int[] elements, int from, int to, int[] temp)
-        {
-            if (from < to)
-            {
-                int middle = (from + to) / 2;
-                mergeSortHelper(elements, from, middle, temp);
-                mergeSortHelper(elements, middle + 1, to, temp);
-                merge(elements, from, middle, to, temp);
-            }
-        }
-        public void merge(int[] arr, int left, int mid, int right, int[] temp)
-        {
-            int i = left;
-            int j= mid+1;
-            int k=left;
-            while(i<= mid&&j<=right)
-            {
-                if(arr[i]<arr[j])
-                {
-                    temp[k]=arr[i];
-                    i++;
-                }
-                else {
-                    temp[k]=arr[j];
-                    j++;
-                }
-                k++;
-            }
-            while(i<=mid)
-            {
-                temp[k]=arr[i];
-                i++;
-                k++;
-            }
-            while(j<=right)
-            {
-                temp[k]=arr[j];
-                j++;
-                k++;
-            }
-            for(int m=left;m<=right;m++)
-            {
-                arr[m]=temp[m];
-            }
-        }
-
+    public int challengeOne(int[] arr) {
 
     }
 
