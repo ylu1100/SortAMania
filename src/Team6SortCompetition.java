9f90be93102c60a79c975ab55ffb94b24f4b3424
clam3154 <clam3154@bths.edy>
2018-11-28 09:47:57 -0500

    so far

--- src/Team6SortCompetition.java
+++ src/Team6SortCompetition.java
@@ -1,3 +1,26 @@
-public class Team6SortCompetition {
+public class Team6SortCompetition extends  SortCompetition{
 
+    public int challengeOne(int[] arr) {
+        return 0;
+    }
+
+    public int challengeTwo(String[] arr, String query) {
+        return 0;
+    }
+
+    public int challengeThree(int[] arr) {
+        return 0;
+    }
+
+    public int challengeFour(int[][] arr) {
+        return 0;
+    }
+
+    public int challengeFive(Comparable[] arr, Comparable query) {
+        return 0;
+    }
+
+    public String greeting() {
+        return null;
+    }
 }
