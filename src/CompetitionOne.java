161767d2239be82b51251c77204e9569a2afcc34
BT_1N3_01 <@>
2018-11-28 09:08:31 -0500

    1

---
+++ src/CompetitionOne.java
@@ -0,0 +1,32 @@
+public class CompetitionOne extends SortCompetition {
+
+    @Override
+    public int challengeOne(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeTwo(String[] arr, String query) {
+        return 0;
+    }
+
+    @Override
+    public int challengeThree(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFour(int[][] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFive(Comparable[] arr, Comparable query) {
+        return 0;
+    }
+
+    @Override
+    public String greeting() {
+        return null;
+    }
+}
