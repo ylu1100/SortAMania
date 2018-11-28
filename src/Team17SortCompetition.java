78410c1cdea8ecdb3968fd780757ab23c4b4ac52
sheba sow <ssow8171@bths.edu>
2018-11-28 09:48:38 -0500

    toString

--- src/Team17SortCompetition.java
+++ src/Team17SortCompetition.java
@@ -2,5 +2,12 @@ public class Team17SortCompetition {
     public Team17SortCompetition(int[] arr)
     {
 
+
+    }
+    public void toString(int[] list) {
+        for (int x = 0; x < list.length; x++) {
+               System.out.println(list[x]);
+        }
+
     }
 }
