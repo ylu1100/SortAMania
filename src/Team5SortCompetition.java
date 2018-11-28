e0dc668c8738d1b83dbf6ea16fb3564b49e6865c
BT_1N3_10 <fg>
2018-11-28 09:19:18 -0500

    Yeah

--- src/Team5SortCompetition.java
+++ src/Team5SortCompetition.java
@@ -2,7 +2,24 @@ public class Team5SortCompetition extends SortCompetition
 {
     public int challengeOne(int[] arr)
     {
-
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            for(int j = i; j >= 0; j--)
+            {
+                if (arr[i] < arr[j])
+                {
+                    arr[i] = a;
+                    arr[j] = b;
+                    arr[i] = b;
+                    arr[j] = a;
+                }
+            }
+        }
+        if(arr.length % 2 > 0)
+            return (arr[arr.length / 2] + arr[(arr.length / 2) + 1]) / 2;
+        return arr[arr.length / 2];
     }
 
     public int challengeTwo(String[] arr, String query)
