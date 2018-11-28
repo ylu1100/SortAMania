73da46c286c1494ea91f9ebacf45e2f500a5d440
BT_1N3_06 <kli8661@bths.edu>
2018-11-28 09:12:25 -0500

    initialized private objects

--- src/Team2SortCompetition.java
+++ src/Team2SortCompetition.java
@@ -1,4 +1,10 @@
 public class Team2SortCompetition {
 
-
+    private int[] challenge1;
+    private String[] challenge2;
+    private String query;
+    private int[] challenge3;
+    private int[][] challenge4;
+    private Comparable[] challenge5;
+    private Comparable cquery;
 }
