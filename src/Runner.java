ec97e674504b9a2b073af0b69ea9da4057332713
sheba sow <ssow8171@bths.edu>
2018-11-28 10:09:20 -0500

    main method

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,8 @@
 public class Runner {
-
+    public static void main(String[] args)
+    {
+        int numCount =  1000;
+        int[] challengeOneList = new int[numCount];
+        Team17SortCompetition.generateIntList(challengeOneList, numCount);
+    }
 }
