5f884e93b6590c2eb5d51e828cf58556f7bb195a
BT_1N3_31 <@>
2018-11-28 09:27:24 -0500

    afueufueufeufuefuefueu

--- src/Team18SortCompetition.java
+++ src/Team18SortCompetition.java
@@ -1,14 +1,35 @@
-public abstract class Team18SortCompetition extends SortCompetition
+public class Team18SortCompetition extends SortCompetition
 {
     public int challengeOne(int[] arr)
     {
-        for(int i = 0; i < arr.length; i++)
-        {
-
-        }
-        int i = 1;
-        return i;
+        return 0;
     }
+
+    @Override
+    public int challengeTwo(String[] arr, String query) {
+        return 0;
+    }
+
+    @Override
+    public int challengeThree(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFour(int[][] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFive(Comparable[] arr, Comparable query) {
+        return 0;
+    }
+
+    @Override
+    public String greeting() {
+        return null;
+    }
+
     public static void intSwap(int[] arr, int pos1, int pos2){
         int temp = arr[pos1];
         arr[pos1] = arr[pos2];
