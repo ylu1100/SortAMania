7bdaad96d8254bf500c7dbcf9a7c7f5539680126
clam3154 <clam3154@bths.edy>
2018-11-28 10:05:25 -0500

    so far

--- src/Team6SortCompetition.java
+++ src/Team6SortCompetition.java
@@ -1,26 +1,62 @@
 public class Team6SortCompetition extends SortCompetition{
 
     public int challengeOne(int[] arr) {
-        return 0;
+        //return median;
     }
 
     public int challengeTwo(String[] arr, String query) {
-        return 0;
+        //return index of the first instance of that string, or -1 if not found;
     }
 
     public int challengeThree(int[] arr) {
-        return 0;
+        //return median;
     }
 
     public int challengeFour(int[][] arr) {
-        return 0;
+        //return median of the median array;
     }
 
     public int challengeFive(Comparable[] arr, Comparable query) {
-        return 0;
+        //return position of the object, or -1 if not found;
     }
 
     public String greeting() {
-        return null;
+        //return any;
+    }
+
+    public void quickSort(int[] arr, int left, int right)
+    {
+        if(left<right)
+        {
+            int pivot = partition(arr,left,right);
+            quickSort(arr,left,pivot-1);
+            quickSort(arr,pivot+1,right);
+        }
+        for(int i = 0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + ",");
+        }
+        System.out.println();
+    }
+
+    public int partition(int[] arr, int left, int right)
+    {
+        int temp;
+        int pivot = arr[right];
+        int i = left-1;
+        for(int j = left; j<right; j++)
+        {
+            if(arr[j] <= pivot)
+            {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        temp = arr[i+1];
+        arr[i+1]=arr[right];
+        arr[right]=temp;
+        return i+1;
     }
 }
