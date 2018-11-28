d1be64ac9ad5c755eba1ca5491c5fada32bff899
BT_1N3_06 <kli8661@bths.edu>
2018-11-28 09:19:12 -0500

    initialized everything

--- src/Team2SortCompetition.java
+++ src/Team2SortCompetition.java
@@ -8,26 +8,60 @@ public abstract class Team2SortCompetition extends SortCompetition{
     private Comparable[] challenge5;
     private Comparable cquery;
 
+    /**
+     * Data Set - an array of 10,000 random integers between 0-10000
+     * Task: Sort the list and return the median
+     * @param challenge1
+     * @return
+     */
     public int challengeOne(int[] challenge1)
     {
 
     }
 
+    /**
+     * Data Set - an array of 10,000 random strings (strings will be of length 5)
+     * Task: Sort the list and determine if it contains a given string, return the index of the first instance of that string, or -1 if not found
+     * @param challenge2
+     * @param query
+     * @return
+     */
     public int challengeTwo(String[] challenge2, String query)
     {
 
     }
 
+    /**
+     * Data Set - a mostly sorted array of 100,000 integers (>75% of elements are in the correct order)
+     * Task: Sort the list and return the median
+     * @param challenge3
+     * @return
+     */
     public int challengeThree(int[] challenge3)
     {
 
     }
 
+    /**
+     * Data Set - a multi-dimensional array int[1000][1000] all elements are random integers between 0-10000
+     * Task: Sort each sub-array and then sort the arrays by their median value
+     * Return the median of the median array
+     * @param challenge4
+     * @return
+     */
     public int challengeFour(int[][] challenge4)
     {
 
     }
 
+    /**
+     * Data Set - an array of 10,000 objects that implement the comparable interface
+     * Task: Sort the array by the compareTo method, and determine if it contains the element given. Return the position of the object, or -1 if not found.
+     * Hint: You must use a stable sort for this challenge, equivalent objects should stay in the same order.
+     * @param challenge5
+     * @param cquery
+     * @return
+     */
     public int challengeFive(Comparable[] challenge5, Comparable cquery)
     {
 
