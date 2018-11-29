b247c5ef9b7ddefc59f18dd1ca76123315c8ce01
as <as>
2018-11-29 09:01:42 -0500

    plz help

--- src/Team18SortCompetition.java
+++ src/Team18SortCompetition.java
@@ -81,6 +81,12 @@ public class Team18SortCompetition extends SortCompetition
 
     @Override
     public int challengeFive(Comparable[] arr, Comparable query) {
+
+
+
+
+
+
         return 0;
     }
 
@@ -154,4 +160,44 @@ public class Team18SortCompetition extends SortCompetition
             doubleSwap(arr, temp, i);
         }
     }
+
+    Team18SortCompetition x = new Team18SortCompetition();
+
+    int[] c1 = generateIntArr(6);
+
+    printIntArray(c1);
+
+    long timer = System.nanoTime();
+
+        System.out.println("median is " + x.challengeOne(c1));
+
+    timer = System.nanoTime() - timer;
+        System.out.println(timer*.000000001 + " seconds");
+
+    printIntArray(c1);
+
+
+
+
+    String[] c2 = {"abcde","cdefe","aaaaa","bbbbb"};
+    printStringArray(c2);
+    timer = System.nanoTime();
+        System.out.println(x.challengeTwo(c2,"abcde"));
+    timer = System.nanoTime() - timer;
+        System.out.println(timer*.000000001 + " seconds");
+
+    printStringArray(c2);
+}
+
+    public static int[] generateIntArr(int x)
+    {
+        int[] z = new int[x];
+        for(int i=0;i<x;i++)
+        {
+            z[i] = (int)(Math.random()*10000);
+        }
+        return z;
+    }
+
+
 }
