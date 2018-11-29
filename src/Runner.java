3fd426dfb26d3e05a3b518c69f7c0953f896c870
BT_1N3_34 <bsmith3816@bths.edu>
2018-11-29 09:30:54 -0500

    11.29 #3

--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public abstract class Runner extends SortCompetition {
 
     public static void main(String[] args){
-        int[] arr1 = randomIntArray(10000, 0, 10000);
+        int[] arr1 = randomIntArr(10000, 0, 10000);
         SortCompetition x = new Team19SortCompetition();
         System.out.println(Arrays.toString(arr1));
         long startTime = System.nanoTime();
@@ -11,17 +11,41 @@ public abstract class Runner extends SortCompetition {
         long endTime = System.nanoTime();
         long duration = endTime - startTime;
         System.out.println(Arrays.toString(arr1));
-        System.out.println("Challenge 1 Duration: " + duration + " nanoseconds");
+        System.out.println("Challenge 1 Duration: " + duration + " nanoseconds \n");
 
-        //String[] arr2 = randomStrArray(1000,5);
+        String[] arr2 = randomStrArr(1000,5);
+        System.out.println(Arrays.toString(arr2));
+        startTime = System.nanoTime();
+        System.out.println(x.challengeTwo(arr2,"bingo"));
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        System.out.println(Arrays.toString(arr2));
+        System.out.println("Challenge 2 Duration: " + duration + " nanoseconds");
+
     }
 
-    public static int[] randomIntArray(int length, int min, int max){
+    public static int[] randomIntArr(int length, int min, int max){
         int[] output = new int[length];
         for(int i = 0; i < length; i++){
             output[i] = getRandomInteger(min,max);
         }
         return output;
+    }
+
+    public static String[] randomStrArr(int num, int length){
+        String[] arr = new String[num];
+        while(num > 0){
+            int i = 0;
+            String s = "";
+            while (i < length){
+                char c = (char)((Math.random()*26) + 65);
+                s += c;
+                i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return arr;
     }
 
     public static int getRandomInteger(int min, int max){
