c87352ca1e5bdd950fd416871a16dc5d0c92b048
BT_1N3_01 <@>
2018-11-29 09:20:45 -0500

    3

--- src/Runner.java
+++ src/Runner.java
@@ -24,34 +24,50 @@ public class Runner
         //for(int i=0;i<arr.length;i++) { System.out.print(arr[i]+" "); }
         System.out.println("\n"+a1.challengeOne(arr));
 
+        System.out.println();
+
+        String[] arr2 = new String[10000];
+        String alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(int i=0;i<arr2.length;i++)
+        {
+            
+        }
+
+
+
+
+
+
+
+
 
 
         System.out.println();
 
 
-        int[] arr2= new int[100000];
-        for(int i=0;i<arr2.length;i++)
+        int[] arr3= new int[100000];
+        for(int i=0;i<arr3.length;i++)
         {
-            arr2[i]=(int)(Math.random()*10000)+0;
+            arr3[i]=(int)(Math.random()*10000)+0;
         }
-        Arrays.sort(arr2);
+        Arrays.sort(arr3);
         for(int i=0;i<200;i++)
         {
-            arr2[i]=(int)(Math.random()*10000)+0;
+            arr3[i]=(int)(Math.random()*10000)+0;
         }
         System.out.print("Unsorted Array: ");
         //for(int i=0;i<arr2.length;i++) { System.out.print(arr2[i]+" "); }
 
-        Team0SortCompetition a2 = new Team0SortCompetition();
+        Team0SortCompetition a3 = new Team0SortCompetition();
 
-        long time2 = System.nanoTime();
+        long time3 = System.nanoTime();
 
-        a2.challengeThree(arr2);
+        a3.challengeThree(arr3);
 
-        System.out.println("\n"+(double)(System.nanoTime() - time2)/(100000000)+" secs");
+        System.out.println("\n"+(double)(System.nanoTime() - time3)/(100000000)+" secs");
 
         System.out.print("Sorted Array: ");
         //for(int i=0;i<arr2.length;i++) { System.out.print(arr2[i]+" "); }
-        System.out.println("\n"+a2.challengeThree(arr2));
+        System.out.println("\n"+a3.challengeThree(arr3));
     }
 }
