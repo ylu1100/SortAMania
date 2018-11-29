e8b66510eaec1ae5cefced3a51420962d3480d16
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-11-29 09:50:17 -0500

    Finished first Method.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/Runner.java
+++ src/Runner.java
@@ -1,12 +1,13 @@
 public class Runner {
     public static void main(String[] args)
     {
-        Team17SortCompetition name = new Team17SortCompetition();
+        Team17SortCompetition c1 = new Team17SortCompetition();
         int numCount =  100;
-        int[] challengeOneList = new int[numCount];
+        int[] challengeOneList;
+
         challengeOneList = Team17SortCompetition.randIntArr(numCount);
         Team17SortCompetition.toString(challengeOneList);
 
-        name.challengeOne(challengeOneList);
+        c1.challengeOne(challengeOneList);
     }
 }
