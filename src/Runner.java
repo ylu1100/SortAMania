477fb3cb56eb6957738718757143343e9e413e57
sheba sow <ssow8171@bths.edu>
2018-11-29 09:50:34 -0500

    challenge One done

--- src/Runner.java
+++ src/Runner.java
@@ -1,12 +1,13 @@
 public class Runner {
     public static void main(String[] args)
     {
-        Team17SortCompetition name = new Team17SortCompetition();
+        Team17SortCompetition cOne = new Team17SortCompetition();
         int numCount =  100;
         int[] challengeOneList = new int[numCount];
         challengeOneList = Team17SortCompetition.randIntArr(numCount);
         Team17SortCompetition.toString(challengeOneList);
 
-        name.challengeOne(challengeOneList);
+        cOne.challengeOne(challengeOneList);
+        System.out.println(cOne.findMed(challengeOneList));
     }
 }
