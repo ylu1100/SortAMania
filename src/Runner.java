89e2eb3046e5202e907fc67b2ce8d70a7da74fe7
BT_1N3_17 <tlee3526@bths.edu>
2018-11-30 09:26:38 -0500

    Hi

---
+++ src/Runner.java
@@ -0,0 +1,20 @@
+public class Runner {
+    public static void main(String [] args)
+    {
+        SortCompetition team1 = new Team1SortCompetition();
+        int[] randIntArr = randomIntsArr(10000);
+        String[] ranStringArr = randomStringArr(10000,5);
+
+        System.out.println("Unsorted");
+        printArr(randIntArr);
+
+        long time= System.currentTimeMillis();
+        int median = team1.challengeOne(randIntArr);
+        time = System.currentTimeMillis() - time;
+        System.out.println("challenge One Time Taken: " + time * 0.001 + "seceonds");
+        System.out.println("Median equals: " + median);
+
+        System.out.println("sorted");
+        printArr(randIntArr);
+    }
+}
