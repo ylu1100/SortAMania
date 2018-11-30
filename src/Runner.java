9a5ff89798a872fa8617eada8868d65b93e30d8f
Afaq Anwar <aanwar9020@bths.edu>
2018-11-30 10:07:01 -0500

    Finished Challenge Two & Challenge Three.
    Signed-off-by: Afaq Anwar <aanwar9020@bths.edu>

--- src/Runner.java
+++ src/Runner.java
@@ -7,4 +7,57 @@
 public class Runner {
     public static void main(String[] args) {
     }
+
+    /**
+     * Generates a random Integer array from 0 - 10,000.
+     * @param count Integer representing the length of the array.
+     * @return 1D Integer Array.
+     */
+    public static int[] randomIntArray(int count) {
+        int[] arr = new int[count];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random() * 10000);
+        }
+        return arr;
+    }
+
+    /**
+     * Generates a random Double array from  0 - 10,000.
+     * @param count Integer representing the length of the array.
+     * @return 1D Double Array.
+     */
+    public static double[] randomDoubleArray(int count) {
+        double[] arr = new double[count];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (Math.random() * 100);
+        }
+        return arr;
+    }
+
+    /**
+     * Generates a random String array with Strings of a certain length.
+     * Upper & Lowercase letters.
+     * @param count Integer representing the length of the array.
+     * @param strLength Integer representing the length of each String inside of the Array.
+     * @return 1D String Array.
+     */
+    public static String[] randomStringArray(int count, int strLength) {
+        String[] arr = new String[count];
+        for (int i = 0; i < arr.length; i++) {
+            int wordLength = strLength;
+            String word = "";
+            for (int x = 0; x < wordLength; x++) {
+                int randomChoice = (int)((Math.random() * 100) + 1);
+                char ch;
+                if (randomChoice > 50) {
+                    ch = (char) ((int) ((Math.random() * 26) + 65));
+                } else {
+                    ch = (char) ((int) ((Math.random() * 26) + 97));
+                }
+                word+= ch;
+            }
+            arr[i] = word;
+        }
+        return arr;
+    }
 }
--- src/Team8SortCompetition.java
+++ src/Team8SortCompetition.java
@@ -1,23 +1,22 @@
+// Afaq Anwar
+
 public class Team8SortCompetition extends SortCompetition {
     @Override
     public int challengeOne(int[] arr) {
-        quickSort(arr, 0, arr.length - 1);
-        int i = ((arr.length + 1)/2) - 1;
-        if (arr.length % 2 == 0) {
-            return (arr[i] + arr[i + 1]) / 2;
-        } else {
-            return arr[i];
-        }
+        quickSortInt(arr, 0, arr.length - 1);
+        return median(arr);
     }
 
     @Override
     public int challengeTwo(String[] arr, String query) {
-        return 0;
+        lsdRadixSortStr(arr, 5);
+        return binarySearchStr(arr, query);
     }
 
     @Override
     public int challengeThree(int[] arr) {
-        return 0;
+        insertionSort(arr);
+        return median(arr);
     }
 
     @Override
@@ -41,11 +40,11 @@ public class Team8SortCompetition extends SortCompetition {
      * @param left Integer representing the starting position of the Array / sector to be sorted. (Begins the sort here - [Usually 0])
      * @param right Integer representing the ending position of the Array / sector to be sorted. (Usually Array.length - 1)
      */
-    private static void quickSort(int[] arr, int left, int right) {
+    private static void quickSortInt(int[] arr, int left, int right) {
         if (left < right) {
-            int pivot = partition(arr, left, right);
-            quickSort(arr, left, pivot - 1);
-            quickSort(arr, pivot + 1, right);
+            int pivot = partitionInt(arr, left, right);
+            quickSortInt(arr, left, pivot - 1);
+            quickSortInt(arr, pivot + 1, right);
         }
     }
     /**
@@ -55,7 +54,7 @@ public class Team8SortCompetition extends SortCompetition {
      * @param right Integer representing the ending position of the partition.
      * @return Integer representing the new "pivot" position.
      */
-    private static int partition(int[] arr, int left, int right) {
+    private static int partitionInt(int[] arr, int left, int right) {
         int pivot = arr[right];
         int i = left - 1;
         for (int j = left; j < right; j++) {
@@ -70,5 +69,92 @@ public class Team8SortCompetition extends SortCompetition {
         arr[right] = arr[i + 1];
         arr[i + 1] = temp;
         return i+ 1;
+    }
+
+    /**
+     * LSD String Sort. Hybrid between a Radix / Count Sort.
+     * Works with a String Array with Strings of an exact length.
+     * @param arr Any 1D String Array.
+     * @param strLength Integer representing the length of each String in the String Array (arr).
+     */
+    private static void lsdRadixSortStr(String[] arr, int strLength) {
+        int arrLength = arr.length;
+        // Maximum ASCII Character.
+        int maxChar = 256;
+        // Temporary Array to store Char frequencies.
+        String[] temp = new String[arrLength];
+        // Loops through each String backwards.
+        for (int i = strLength-1; i >= 0; i--) {
+            // Sets up an Array that will store the frequencies of words.
+            int[] count = new int[maxChar + 1];
+            // Calculates frequencies.
+            for (int j = 0; j < arrLength; j++) {
+                count[arr[j].charAt(i) + 1]++;
+            }
+            // Add up frequencies.
+            for (int k = 0; k < maxChar; k++) {
+                count[k + 1] += count[k];
+            }
+            // Move into place in temp Array.
+            for (int j = 0; j < arrLength; j++) {
+                temp[count[arr[j].charAt(i)]++] = arr[j];
+            }
+            // Original Array is now modified to reflect temp Array.
+            for (int j = 0; j < arrLength; j++) {
+                arr[j] = temp[j];
+            }
+        }
+    }
+
+    /**
+     * Optimized Insertion Sort.
+     * @param arr Any 1D Integer Array.
+     */
+    private static void insertionSort (int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int currHold = arr[i];
+            int currPos = i;
+            while (currPos > 0 && arr[currPos -1] > currHold) {
+                arr[currPos] = arr[currPos - 1];
+                currPos--;
+            }
+            arr[currPos] = currHold;
+        }
+    }
+
+    /**
+     * Calculates the median.
+     * @param arr Any 1D Integer Array.
+     * @return Integer representing the median. (Rounded Down)
+     */
+    private static int median(int[] arr){
+        int i = ((arr.length + 1)/2) - 1;
+        if (arr.length % 2 == 0) {
+            return (arr[i] + arr[i + 1]) / 2;
+        } else {
+            return arr[i];
+        }
+    }
+
+    /**
+     * Binary Search for a String in an Array of Strings.
+     * @param arr Any 1D String Array.
+     * @param key String to be searched for.
+     * @return Integer representing the position of the String in the Array. (-1 if not found)
+     */
+    public static int binarySearchStr(String[] arr, String key) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (key.compareTo(arr[middle]) < 0) {
+                right = middle - 1;
+            } else if (key.compareTo(arr[middle]) > 0) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
     }
 }
