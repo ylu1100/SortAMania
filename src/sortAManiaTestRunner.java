5e8fa0a3dba4c3dd0c25462b279e3486a1c75bd4
clam3154 <clam3154@bths.edu>
2018-12-02 16:44:02 -0500

    so far

--- src/sortAManiaTestRunner.java
+++ src/sortAManiaTestRunner.java
@@ -1,10 +1,12 @@
+import java.util.Random;
+
 public class sortAManiaTestRunner
 {
     public static void main(String[] args)
     {
         SortCompetition team6 = new Team6SortCompetition();
         int[] randIntArr = randomIntsArr(10000);
-        String randStringArr = randomStringArr(10000, 5);
+        String[] randStringArr = randomStringArr(10000, 5);
 
         System.out.println("Unsorted");
         printArr(randIntArr);
@@ -26,5 +28,29 @@ public class sortAManiaTestRunner
             arr[i] = (int)(Math.random()*10000);
         }
         return arr;
+    }
+
+    public static String[] randomStringArr(int num, int length)
+    {
+        String[] randomStrings = new String[num];
+        Random random = new Random();
+        for(int i = 0; i < num; i++)
+        {
+            char[] word = new char[random.nextInt(length)];
+            for(int j = 0; j < word.length; j++)
+            {
+                word[j] = (char)('a' + random.nextInt(26));
+            }
+            randomStrings[i] = new String(word);
+        }
+        return randomStrings;
+    }
+
+    public static void printArr(int[] arr)
+    {
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.println();
+        }
     }
 }
