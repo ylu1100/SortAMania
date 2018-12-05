ea8e9bd7bb530dcf208fd466a1a179c0d4f17928
acox9567 <43242730+acox9567@users.noreply.github.com>
2018-12-05 09:06:07 -0500

    Update SortCompetition.java

--- src/SortCompetition.java
+++ src/SortCompetition.java
@@ -2,17 +2,17 @@
 //Name your class Team#SortCompetition.
 //e.g. Team 1 should name their class Team1SortCompetition
 public abstract class SortCompetition {
-
+	
 	public abstract int challengeOne(int[] arr);
-
+	
 	public abstract int challengeTwo(String[] arr, String query);
-
+	
 	public abstract int challengeThree(int[] arr);
-
+	
 	public abstract int challengeFour(int[][] arr);
-
+	
 	public abstract int challengeFive(Comparable[] arr, Comparable query);
-
+	
 	//Add a custom greeting so your sorter can introduce itself
 	public abstract String greeting();
 }
