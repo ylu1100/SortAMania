9be88bc3b20422ea6e1a5c4a5f2781cbda932a3b
dcordova7146 <43242715+dcordova7146@users.noreply.github.com>
2018-12-04 23:41:00 -0500

    Update SortCompetition.java

--- src/SortCompetition.java
+++ src/SortCompetition.java
@@ -1,15 +1,12 @@
 //Implement this abstract class in your own class in this project.
-//Name your class Team#SortCompetition.
-//e.g. Team 1 should name their class Team1SortCompetition
 public abstract class SortCompetition {
-
 	
 	public abstract int challengeOne(int[] arr);
 	
 	public abstract int challengeTwo(String[] arr, String query);
 	
 	public abstract int challengeThree(int[] arr);
-
+	
 	public abstract int challengeFour(int[][] arr);
 	
 	public abstract int challengeFive(Comparable[] arr, Comparable query);
