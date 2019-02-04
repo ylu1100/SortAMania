9905b708fc1221f832dab07e9abeb1888fc4614e
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2019-02-04 08:24:53 -0500

    Update Team5SortCompetition.java

--- src/Team5SortCompetition.java
+++ src/Team5SortCompetition.java
@@ -1,3 +1,33 @@
 
-public class Team5SortCompetition {
+
+public class Team5SortCompetition extends SortCompetition{
+    @Override
+    public int challengeOne(int[] arr) {
+        
+    }
+
+    @Override
+    public int challengeTwo(String[] arr, String query) {
+        return 0;
+    }
+
+    @Override
+    public int challengeThree(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFour(int[][] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFive(Comparable[] arr, Comparable query) {
+        return 0;
+    }
+
+    @Override
+    public String greeting() {
+        return null;
+    }
 }
