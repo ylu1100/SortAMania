b5a007bd022bdab603185a83c7635a17b109a522
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2019-02-06 08:41:52 -0500

    Update Team5SortCompetition.java

--- src/Team5SortCompetition.java
+++ src/Team5SortCompetition.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 
 
@@ -37,7 +36,18 @@ public class Team5SortCompetition extends SortCompetition{
 
     @Override
     public int challengeThree(int[] arr) {
-        return 0;
+        int[]newarray=new int [arr.length/2];
+        int[]newarray2=new int[arr.length-newarray.length];
+        int[]combnewarray=new int[arr.length];
+        for(int i = 0;i<arr.length/2;i++){
+            newarray[i]=arr[i];
+        }
+        for(int x = arr.length/2;x<arr.length;x++){
+            newarray2[x]=arr[x];
+        }
+        SortingUtils.insertionSort(newarray);
+        SortingUtils.insertionSort(newarray2);
+        
     }
 
     @Override
