b8120abeef4601c1fe0d95fe4f1a50e0fd441dbd
jzhang0688 <43207462+jzhang0688@users.noreply.github.com>
2019-02-06 22:56:32 -0500

    mergeSort

--- src/Team5SortCompetition.java
+++ src/Team5SortCompetition.java
@@ -203,5 +203,59 @@ public class Team5SortCompetition extends SortCompetition{
             arr[x]=combarray[x];
         }
     }
+
+    public class MyMergeSort {
+
+        private int[] arr;
+        private int[] temp;
+        private int length;
+
+
+        public void sort(int inputArr[]) {
+            this.arr = inputArr;
+            this.length = inputArr.length;
+            this.temp = new int[length];
+            doMergeSort(0, length - 1);
+        }
+
+        private void doMergeSort(int lowerIndex, int higherIndex) {
+
+            if (lowerIndex < higherIndex) {
+                int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+                // Below step sorts the left side of the array
+                doMergeSort(lowerIndex, middle);
+                // Below step sorts the right side of the array
+                doMergeSort(middle + 1, higherIndex);
+                // Now merge both sides
+                mergeParts(lowerIndex, middle, higherIndex);
+            }
+        }
+
+        private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+
+            for (int i = lowerIndex; i <= higherIndex; i++) {
+                temp[i] = arr[i];
+            }
+            int i = lowerIndex;
+            int j = middle + 1;
+            int k = lowerIndex;
+            while (i <= middle && j <= higherIndex) {
+                if (temp[i] <= temp[j]) {
+                    arr[k] = temp[i];
+                    i++;
+                } else {
+                    arr[k] = temp[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i <= middle) {
+                arr[k] = temp[i];
+                k++;
+                i++;
+            }
+
+        }
+    }
 }
 
